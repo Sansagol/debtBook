2644234776aced747715332a9f225efa8edb07f4
Kirill Markov <dev.sansagol@gmail.com>
2017-05-21 14:10:24 +0300

    Adding a base of event behaviour.

---
+++ DebtBook/DebtBook/Core/Behaviours/EventToCommand.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace DebtBook.Core.Behaviours
+{
+    class EventToCommand: Behavior<View>
+    {
+        public static readonly BindableProperty CommandProperty =
+            BindableProperty.Create(
+                "Command",
+                typeof(ICommand),
+                typeof(EventToCommand));
+        public static readonly BindableProperty EventNameProperty =
+            BindableProperty.Create(
+                "EventName",
+                typeof(string),
+                typeof(EventToCommand));
+
+
+
+        public ICommand Command
+        {
+            get { return (ICommand)GetValue(CommandProperty); }
+            set { SetValue(CommandProperty, value); }
+        }
+
+        public string EventName
+        {
+            get { return (string)GetValue(EventNameProperty); }
+            set { SetValue(EventNameProperty, value); }
+        }
+
+        protected override void OnAttachedTo(View bindable)
+        {
+            base.OnAttachedTo(bindable);
+        }
+
+        protected override void OnDetachingFrom(View bindable)
+        {
+            base.OnDetachingFrom(bindable);
+        }
+
+        private void Register(string eventName)
+        {
+            if (string.IsNullOrWhiteSpace(eventName))
+                throw new ArgumentNullException(nameof(eventName));
+        }
+    }
+}
